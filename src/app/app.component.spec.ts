1c3121228b5475245df94bdd444af6edf49dcf36
llOrTeGall <IvanOrtega_97@hotmail.com>
2023-04-03 14:46:48 -0500

    desde 0 again

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'learning_app_angular'`, () => {
+  it(`should have as title 'apps'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('learning_app_angular');
+    expect(app.title).toEqual('apps');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('learning_app_angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('apps app is running!');
   });
 });
